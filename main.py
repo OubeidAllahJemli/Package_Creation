74482bd7504d2a59f5b68b0498a870232e33dcb3
OubeidAllahJemli <jemlioubeidallah@gmail.com>
2026-01-21 14:58:45 +0100

    adding sphinx_doc file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from package_creation_tutorial.string_ops import (reverse_string, 
                                                   count_vowels, capitalize_words)
 
+
 def main() -> None:
     """
     Main function to demonstrate the string operations.
